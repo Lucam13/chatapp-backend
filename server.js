e991f43fc7b4d2528ccdcaf8df04dad309fa2af8
Lucam13 <lucc_88@hotmail.com>
2024-08-20 20:23:47 -0300

    added gitignore and package

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ const __dirname = path.resolve();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
+app.use(
+  cors({
+    origin: "https://epar-mensajeria.vercel.app",
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
+
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 app.use(morgan("dev"));
@@ -30,10 +38,10 @@ app.use("/api/areas", areaRoutes);
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
-	connectToMongoDB();
-	console.log(`Server Running on port ${PORT}`);
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
 });
