cbe00fd4aec3fb76ea0079172263e3e0afac2987
Lucam13 <lucc_88@hotmail.com>
2024-08-20 21:32:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
